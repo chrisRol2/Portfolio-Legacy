e5139a05121f13eabe7a0073f08ee03ef60943af
Christopher Roldán Sánchez <Christopher_rol@hotmail.es>
2021-07-30 21:25:59 -0300

    delete bugs

--- portfolio/src/components/ContactMe.js
+++ portfolio/src/components/ContactMe.js
@@ -8,10 +8,7 @@ export class ContactMe extends Component {
                     <h2>Contact Me</h2>
                 </div>
                 <div id="contact__div">
-                    <div className="project_N"></div>
-                    <div className="project_N"></div>
-                    <div className="project_N"></div>
-                    <div className="project_N"></div>
+                    
                 </div>
             </div>
         )
