abfcd180f230af68163e8d2bb74ed482e05e763b
Christopher Roldán Sánchez <Christopher_rol@hotmail.es>
2021-08-07 11:59:57 -0300

    add links

--- portfolio/src/components/NavBar.js
+++ portfolio/src/components/NavBar.js
@@ -1,33 +1,58 @@
-import React, { Component } from 'react'
-import './styles/NavBar.css'
-import './styles/animate.min.css';
+import React, { Component } from "react";
+import "./styles/NavBar.css";
+import "./styles/animate.min.css";
 export class NavBar extends Component {
-    state = {
-        
-    }
-    onClick = (e) => {
-        this.setState({
-            [e.target.attributes.name.nodeValue]: 'animate__animated animate__rubberBand' })
-        setTimeout(() => { this.setState({ [e.target.attributes.name.nodeValue]: '' }) }, 1000)
-    }
-    render() {
-
-        return (
-            <div id="navbar">
-                <h2 name="Home" className={this.state.Home}
-                    onClick={this.onClick}
-                >Home</h2>
-                <h2 name="About" className={this.state.About}
-                    onClick={this.onClick}>About</h2>
-                <h2 name="Projects" className={this.state.Projects}
-                    onClick={this.onClick}>Works</h2>
-                <h2 name="cv" className={this.state.cv}
-                    onClick={this.onClick}>CV</h2>
-                <h2 name="Contact" className={this.state.Contact}
-                    onClick={this.onClick}>Contact</h2>
-            </div>
-        )
-    }
+  state = {};
+  onClick = (e) => {
+    this.setState({
+      [e.target.attributes.name.nodeValue]:
+        "animate__animated animate__rubberBand",
+    });
+    setTimeout(() => {
+      this.setState({ [e.target.attributes.name.nodeValue]: "" });
+    }, 1000);
+  };
+  render() {
+    return (
+      <nav id="navbar">
+        <a
+          href="#header"
+          name="Home"
+          className={this.state.Home}
+          onClick={this.onClick}
+        >
+          Home
+        </a>
+        <a
+          href="#about"
+          name="About"
+          className={this.state.About}
+          onClick={this.onClick}
+        >
+          About
+        </a>
+        <a
+          href="#projects"
+          name="Projects"
+          className={this.state.Projects}
+          onClick={this.onClick}
+        >
+          Works
+        </a>
+        <a href="/#" name="cv" className={this.state.cv} onClick={this.onClick}>
+          CV
+        </a>
+        <a
+          href="#contact"
+          name="Contact"
+          className={this.state.Contact}
+          onClick={this.onClick}
+        >
+          Contact
+        </a>
+      </nav>
+    );
+  }
 }
 
-export default NavBar
+export default NavBar;
