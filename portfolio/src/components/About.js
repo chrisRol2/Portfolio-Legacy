773165b004134b2cc48528fa127b39b18dd79b14
Christopher Roldán Sánchez <Christopher_rol@hotmail.es>
2021-08-08 01:43:25 -0300

    add hooks

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -1,30 +1,28 @@
-import React, { Component } from "react";
+import React from "react";
 import "./styles/About.css";
 import profile from "../images/images.png";
 import MeAbout from "../API/AboutMe.json";
 
-export class About extends Component {
-  render() {
-    return (
-      <div id="about">
-        <div className="title__component">
-          <h2>About Me</h2>
+const About = () => {
+  return (
+    <div id="about" scrollspy="true">
+      <div className="title__component" id="about__Title">
+        <h2>About Me</h2>
+      </div>
+      <div id="aboutMe">
+        <div id="aboutMe__image-container">
+          <img src={profile} id="image" alt="Profile"></img>
         </div>
-        <div id="aboutMe">
-          <div id="aboutMe__image-container">
-            <img src={profile} id="image" alt="Profile"></img>
-          </div>
 
-          <div id="aboutMe__about-container">
-            <h2>FrontEnd Developer</h2>
-            <br />
-            <h2>Christopher Roldan Sanchez</h2>
-            <p>{MeAbout.description}</p>
-          </div>
+        <div id="aboutMe__about-container">
+          <h2>FrontEnd Developer</h2>
+          <br />
+          <h2>Christopher Roldan Sanchez</h2>
+          <p>{MeAbout.description}</p>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default About;
