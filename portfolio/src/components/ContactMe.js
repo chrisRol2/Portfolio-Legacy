6222ee4ed84fb1ebadc30fb4425a2220002c4022
Christopher Roldán Sánchez <Christopher_rol@hotmail.es>
2021-08-08 01:44:58 -0300

    Add Hooks

--- portfolio/src/components/ContactMe.js
+++ portfolio/src/components/ContactMe.js
@@ -1,16 +1,15 @@
-import React, { Component } from "react";
+import React, {  } from "react";
 import "./styles/ContactMe.css";
-export class ContactMe extends Component {
-  render() {
-    return (
-      <div id="contact">
-        <div className="title__component">
-          <h2>Contact Me</h2>
-        </div>
-        <div id="contact__div"></div>
+
+const ContactMe = () => {
+  return (
+    <div id="contact" scrollspy="true">
+      <div className="title__component" id="contact__Title">
+        <h2>Contact Me</h2>
       </div>
-    );
-  }
-}
+      <div id="contact__div"></div>
+    </div>
+  );
+};
 
 export default ContactMe;
