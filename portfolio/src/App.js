b478007d748c6d12c7ebd1dde1d9fa4b53a7cdd7
Christopher Roldán Sánchez <Christopher_rol@hotmail.es>
2021-08-09 00:50:57 -0300

    remove <NavBar/> and mediaquerys

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,31 +1,18 @@
-import React, { useState, useEffect } from "react";
-import NavBar from "./components/NavBar";
+import React, {  } from "react";
 import Header from "./components/Header";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/ContactMe";
 import "./App.css";
-import { firstLoad } from "./helpers/scrollSpy";
 import { MediaProvider } from "./context/MediaQueryContext";
 // animate__backInUp
 const App = () => {
-    // const { mediaMatch } = useContext(MediaContext);
-  const [mediaQuery, setMediaQuery] = useState(false);
-  const mediaQueryEvent = matchMedia("(min-width: 450px)");
-  useEffect(() => {
-    firstLoad();
-    setMediaQuery(mediaQueryEvent.matches);
-    mediaQueryEvent.addEventListener("change", () => {
-      setMediaQuery(mediaQueryEvent.matches);
-    });
-  }, []);
+  
 
   return (
     <div>
       <MediaProvider>
         <Header />
-        {mediaQuery && <NavBar />}
-        
         <About />
         <Projects />
         <Contact />
