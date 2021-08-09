0907afcf65fa670cc920be6d3b68d8a50506aacf
Christopher Roldán Sánchez <Christopher_rol@hotmail.es>
2021-08-09 00:48:30 -0300

    create mediaQuerys context

--- portfolio/src/context/MediaQueryContext.js
+++ portfolio/src/context/MediaQueryContext.js
@@ -1,10 +1,21 @@
-import { createContext } from "react";
+import { useState, createContext } from "react";
 
 const MediaContext = createContext();
 
 const MediaProvider = ({ children }) => {
+  const matchMediaEvent = matchMedia("(min-width: 518px)");
+
+  const [mediaMatch, setMediaMatch] = useState(matchMediaEvent.matches);
+  const [resolution, setResolution] = useState(1024);
+
+  matchMediaEvent.addEventListener("change", () => {
+    setResolution(window.innerWidth);
+    setMediaMatch(matchMediaEvent.matches);
+  });
+
   const data = {
-    mediaMatch: "",
+    mediaMatch: mediaMatch,
+    resolution: resolution,
   };
 
   return <MediaContext.Provider value={data}>{children}</MediaContext.Provider>;
