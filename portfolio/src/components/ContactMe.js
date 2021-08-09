2216372769921eb0285fb119fee083ef5f57d4b6
Christopher Roldán Sánchez <Christopher_rol@hotmail.es>
2021-08-09 17:00:42 -0300

    add data checks

--- portfolio/src/components/ContactMe.js
+++ portfolio/src/components/ContactMe.js
@@ -1,17 +1,72 @@
-import React, { useState } from "react";
+import React, { useState, useContext, useEffect } from "react";
+import ErrorContext from "../context/ErrorContext";
 import "./styles/ContactMe.css";
+const idleStyles = {
+  style: {
+    color: "#121111",
+    borderColor: "#121111",
+  },
+  class: "form__input",
+  classArea: "form__input-textarea",
+};
+const errorStyles = {
+  style: {
+    // color: "#f66",
+    borderColor: "#f66",
+  },
+  class: " animate__animated animate__shakeX form__input",
+  classArea:
+    "form__input-textarea animate__animated animate__shakeX form__input",
+};
+// const sentStyles = {
+//   style: {},
+//   class: "form__input",
+// };
+
+const styleForm = {
+  email: idleStyles,
+  name: idleStyles,
+  text: idleStyles,
+};
+
 const ContactMe = () => {
-  const [name, setName] = useState(null);
+  const [name, setName] = useState("");
   const [email, setEmail] = useState(null);
-  const [text, setText] = useState(null);
-  let emailExpression =
-  /^(?:[^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*|"[^\n"]+")@(?:[^<>()[\].,;:\s@"]+\.)+[^<>()[\]\.,;:\s@"]{2,63}$/i;
-  
+  const [text, setText] = useState("");
+  const [errorStyle, setErrorStyle] = useState(styleForm);
+  const { error, setError } = useContext(ErrorContext);
+  const emailExpression =
+    /^(?:[^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*|"[^\n"]+")@(?:[^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,63}$/i;
+  const nameExpression = /[A-Z-a-z-À-ÿ] /;
+
+  const textExpression = /[A-Z-a-z-À-ÿ] /;
+  const handleChange = (e) => {
+    if (e.target.name === "nameEMail") {
+      setErrorStyle({ ...errorStyle, name: idleStyles });
+    }
+    if (e.target.name === "emailEMail") {
+      setErrorStyle({ ...errorStyle, email: idleStyles });
+    }
+    if (e.target.name === "textEMail") {
+      setErrorStyle({ ...errorStyle, text: idleStyles });
+    }
+  };
+
   const post = () => {
-    //TODO: agregar control de que no sean null y tengan algo importante
-    if (emailExpression.test(email)) {
-      console.log("enviando");
-      fetch("https://formsubmit.co/ajax/christopher_rol@hotmail.es", {
+    let errorFlag = false;
+    let errorName = name.split(" ").length > 1 && nameExpression.test(name);
+    let errorMail = emailExpression.test(email);
+    let errorText = text.split(" ").length > 15 && textExpression.test(text);
+    if (!errorName) errorFlag = true;
+    if (!errorMail) errorFlag = true;
+    if (!errorText) errorFlag = true;
+
+    if (!errorFlag) {
+      setError({
+        error: null,
+      });
+
+      fetch("https://formsubmit.cos/ajax/christopher_rol@hotmail.es", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -25,12 +80,48 @@ const ContactMe = () => {
         }),
       })
         .then((response) => response.json())
-        // .then((data) => console.log(data))
-        .catch((error) => console.log(error));
-      return;
+        .then((data) => {
+          setError({
+            errorForm: !data.success ? "Fail to send email" : "ok",
+          });
+        })
+        .catch((err) => {
+          setError({
+            errorForm: "connection error",
+          });
+        });
     } else {
+      //errorStyle
+      setErrorStyle({
+        name: !errorName ? errorStyles : idleStyles,
+        email: !errorMail ? errorStyles : idleStyles,
+        text: !errorText ? errorStyles : idleStyles,
+      });
+      setTimeout(() => {
+        setErrorStyle({
+          name: !errorName
+            ? { ...errorStyles, class: "form__input" }
+            : idleStyles,
+          email: !errorMail
+            ? { ...errorStyles, class: "form__input" }
+            : idleStyles,
+          text: !errorText
+            ? { ...errorStyles, classArea: "form__input-textarea" }
+            : idleStyles,
+        });
+      }, 1000);
+      setError({ errorForm: "invalid email" });
+      return;
     }
   };
+
+  useEffect(() => {
+    if (error.errorForm === "invalid email") {
+    } else if (error.errorForm === "Fail to send email") {
+    } else if (error.errorForm === "connection error") {
+    } else if (error.errorForm === "ok") {
+    }
+  }, [error]);
 
   return (
     <div id="contact" scrollspy="true">
@@ -41,9 +132,13 @@ const ContactMe = () => {
         <div className="form__input__div">
           <label htmlFor="name-email">Name</label>
           <input
-            onChange={(e) => setName(e.target.value)}
+            style={errorStyle.name.style}
+            className={errorStyle.name.class}
+            onChange={(e) => {
+              setName(e.target.value);
+              handleChange(e);
+            }}
             placeholder="Name"
-            className="form__input"
             type="text"
             name="nameEMail"
             id="name-email"
@@ -53,20 +148,28 @@ const ContactMe = () => {
         <div className="form__input__div">
           <label htmlFor="email-email">Email</label>
           <input
+            style={errorStyle.email.style}
+            className={errorStyle.email.class}
             placeholder="Email"
-            className="form__input"
             type="email"
             name="emailEMail"
             id="email-email"
-            onChange={(e) => setEmail(e.target.value)}
+            onChange={(e) => {
+              setEmail(e.target.value);
+              handleChange(e);
+            }}
           />
         </div>
         <div className="form__input__div">
           <label htmlFor="text-email">Message</label>
 
           <textarea
-            onChange={(e) => setText(e.target.value)}
-            className="form__input-textarea"
+            onChange={(e) => {
+              setText(e.target.value);
+              handleChange(e);
+            }}
+            style={errorStyle.text.style}
+            className={errorStyle.text.classArea}
             name="textEMail"
             id="text-email"
             cols="30"
