7cf2922dae3b245d389c10c6f4aa6869abb12325
Christopher Roldán Sánchez <Christopher_rol@hotmail.es>
2021-08-12 00:12:17 -0300

    Add menu

--- portfolio/src/components/NavBar.js
+++ portfolio/src/components/NavBar.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import "./styles/NavBar.css";
 import "./styles/animate.min.css";
-import scrollSpy, {  } from "../helpers/scrollSpy";
+import scrollSpy from "../helpers/scrollSpy";
+import "./styles/hamburgers.css";
 export class NavBar extends Component {
   state = {
     mediaQuery: false,
@@ -9,16 +10,18 @@ export class NavBar extends Component {
   };
   onClick = (e) => {
     this.setState({
-      [e.target.attributes.name.nodeValue]:
-        "animate__animated animate__rubberBand active",
+      [e.target.attributes.name.nodeValue]: "animate__rubberBand active ",
     });
     setTimeout(() => {
-      this.setState({ [e.target.attributes.name.nodeValue]: "active" });
+      this.setState({
+        [e.target.attributes.name.nodeValue]: "active ",
+      });
     }, 1000);
   };
   componentDidMount = () => {
-    this.setState({ mediaQuery: matchMedia("(min-width: 425px)").matches });
-    if (this.state.mediaQuery) this.setState({ scroll: scrollSpy() });
+    // this.setState({ mediaQuery: matchMedia("(min-width: 425px)").matches });
+    // // if (this.state.mediaQuery) this.setState({ scroll: scrollSpy() });
+    scrollSpy();
   };
   componentWillUnmount = () => {
     // unObserver(this.state.scroll);
@@ -30,7 +33,7 @@ export class NavBar extends Component {
           <a
             href="#header"
             name="Home"
-            className={this.state.Home}
+            className={"nav__link animate__animated " + this.state.Home}
             onClick={this.onClick}
           >
             Home
@@ -38,7 +41,7 @@ export class NavBar extends Component {
           <a
             href="#about"
             name="About"
-            className={this.state.About}
+            className={"nav__link animate__animated " + this.state.About}
             onClick={this.onClick}
           >
             About
@@ -46,22 +49,31 @@ export class NavBar extends Component {
           <a
             href="#projects"
             name="Projects"
-            className={this.state.Projects}
+            className={"nav__link animate__animated " + this.state.Projects}
             onClick={this.onClick}
           >
             Works
           </a>
-          {/* <a href="/#" name="cv" className={this.state.cv} onClick={this.onClick}>
+          {/* <a href="/#" name="cv" className={["nav__link animate__animated " +this.state.cv]} onClick={this.onClick}>
           CV
         </a> */}
           <a
             href="#contact"
             name="Contact"
-            className={this.state.Contact}
+            className={"nav__link animate__animated " + this.state.Contact}
             onClick={this.onClick}
           >
             Contact
           </a>
+          <div id="nav__resposive">
+            <div></div>
+            <a href="#header">Christopher</a>
+            <button className="hamburger hamburger--spring" type="button">
+              <span className="hamburger-box">
+                <span className="hamburger-inner"></span>
+              </span>
+            </button>
+          </div>
         </>
       </nav>
     );
