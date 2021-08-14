192019e317201fd1bc5d25b99cca84f4a707721b
Christopher Roldán Sánchez <Christopher_rol@hotmail.es>
2021-08-14 00:15:56 -0300

    added items

--- portfolio/src/components/Menu.js
+++ portfolio/src/components/Menu.js
@@ -1,7 +1,18 @@
 import React from "react";
 import "./styles/Menu.css";
-function Menu() {
-  return <div></div>;
+function Menu(props) {
+  return (
+    <div id="menu" style={props.style}>
+      <div id="menu__items">
+        <div className="menu__item">Home</div>
+        <div className="menu__item">About</div>
+        <div className="menu__item">Tools</div>
+        <div className="menu__item">Works</div>
+        <div className="menu__item">Contact</div>
+      </div>
+      <div id="menu__config">config</div>
+    </div>
+  );
 }
 
 export default Menu;
