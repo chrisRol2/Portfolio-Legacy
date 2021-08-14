fda4eb57290304e4821605641ff11edc43c931ee
Christopher Roldán Sánchez <Christopher_rol@hotmail.es>
2021-08-14 00:15:40 -0300

    Update App.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -7,12 +7,14 @@ import "./App.css";
 import { MediaProvider } from "./context/MediaQueryContext";
 import { ErrorProvider } from "./context/ErrorContext";
 import Techno from "./components/Techno";
+
 // animate__backInUp
 const App = () => {
   return (
     <div>
       <MediaProvider>
         <Header />
+
         <About />
         <Techno />
         <Projects />
